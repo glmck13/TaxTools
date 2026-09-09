fc72f6c367aa76e420acd7658a023510a64cf9c7
glmck13 <glmck13@verizon.net>
2026-09-09 19:31:27 -0400

    Update qbota.sh

--- Web/qbota.sh
+++ Web/qbota.sh
@@ -11,17 +11,3 @@ source /var/www/webenv/bin/activate
 
 export GEMINI_API_KEY=""
 qbota.py $*
-
-python3 <<EOF
-from google import genai
-import os
-
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-
-for f in client.files.list():
-    try:
-        client.files.delete(name=f.name)
-        print(f"Deleted: {f.name}")
-    except Exception as e:
-        print(f"Failed to delete {f.name}: {e}")
-EOF
